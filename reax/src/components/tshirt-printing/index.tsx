6db0b7aa751d206ffe9a8815e48accbd27eedb8c
Prince <thedrkprns@gmail.com>
2026-09-11 13:52:22 +0530

    fix of atc error and release tpplus

--- reax/src/components/tshirt-printing/index.tsx
+++ reax/src/components/tshirt-printing/index.tsx
@@ -10,15 +10,32 @@ if (preactRoot) {
     actionSubmit,
     actionClose,
     actionRemove,
+    actionAddSecondLine,
+    actionClearSecondLine,
     headingPersonalise,
     labelTitle,
     labelInput,
+    labelSecondInput,
     labelNote,
     labelSummaryHtml,
     messageInvalidInput,
+    messageLogoUnavailable,
+    messageLogoLoading,
+    countryIn,
+    countryId,
+    countryCn,
+    countryJp,
+    countryMy,
+    countryDk,
+    liningLogoSrc,
+    hndrdLogoSrc,
     closeConfirmMessage,
     closeConfirmKeepEditing,
     closeConfirmDiscard,
+    secondLineEnabled,
+    singleLinePrice,
+    plusPrice,
+    clearIconSrc,
   } = preactRoot.dataset;
 
   render(
@@ -29,21 +46,41 @@ if (preactRoot) {
         submit: actionSubmit || '',
         close: actionClose || '',
         remove: actionRemove || '',
+        addSecondLine: actionAddSecondLine || '',
+        clearSecondLine: actionClearSecondLine || '',
       }}
       labels={{
         heading: headingPersonalise || '',
         title: labelTitle || '',
         input: labelInput || '',
+        secondInput: labelSecondInput || '',
         note: labelNote || '',
         summaryHtml: labelSummaryHtml || '',
       }}
       messages={{
         invalidInput: messageInvalidInput || '',
+        logoUnavailable: messageLogoUnavailable || '',
+        logoLoading: messageLogoLoading || '',
       }}
       closeConfirm={{
         message: closeConfirmMessage || '',
         keepEditing: closeConfirmKeepEditing || '',
         discard: closeConfirmDiscard || '',
+      }}
+      secondLine={{
+        enabled: secondLineEnabled === 'true',
+        singleLinePrice: singleLinePrice || '',
+        plusPrice: plusPrice || '',
+        clearIconSrc: clearIconSrc || '',
+        logoUrls: { lining: liningLogoSrc || '', hndrd: hndrdLogoSrc || '' },
+        countryLabels: {
+          IN: countryIn || '',
+          ID: countryId || '',
+          CN: countryCn || '',
+          JP: countryJp || '',
+          MY: countryMy || '',
+          DK: countryDk || '',
+        },
       }}
     />,
     preactRoot,
